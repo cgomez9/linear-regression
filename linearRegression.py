608c03652765d7f4dabbb0dacba49baf041e47b7
Carlos Gomez <salos_12@hotmail.com>
2018-04-07 21:55:42 -0400

    Examples parse

--- linearRegression.py
+++ linearRegression.py
@@ -1,7 +1,20 @@
 class LinearRegression:
 
-    def __init__(self):
-        pass
+    def __init__(self,outputFile):
+        self.weigthVectors = np.array([0,0,0])
+        self.exampleVector = []
+        self.trueLabels = []
+        self.outputFile = outputFile
 
     def train(self,features):
-        pass
+        with open(self.outputFile, 'w') as csvfile:
+            spamwriter = csv.writer(csvfile, lineterminator='\n')
+            if len(examples) > 0:
+                for example in examples:
+                    features = example.split(',')
+                    self.exampleVector.append((1,int(features[0]),int(features[1])))
+                    self.trueLabels.append(int(features[2]))
+                while(True):
+                    pass
+            else:
+                print("No features provided.")
